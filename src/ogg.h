67d80838d04a8cebbc038bd9b8c565a7b77bd644
Tyler Dahl <tyldahl@gmail.com>
2026-02-18 00:47:30 -0500

    Add ogg example

--- src/ogg.h
+++
@@ -1,5 +0,0 @@
-#ifndef OPENAL_TUTORIAL_OGG_H
-#define OPENAL_TUTORIAL_OGG_H
-
-void ogg_example();
-#endif
--- src/wav.h
+++
@@ -1,5 +0,0 @@
-#ifndef OPENAL_TUTORIAL_WAV_H
-#define OPENAL_TUTORIAL_WAV_H
-
-void wav_example();
-#endif
